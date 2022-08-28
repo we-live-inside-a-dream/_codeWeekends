ddfcdcc84cee6ec7e04c989f7be9dc4e7249eccf
reza <rezanaeimabadi7@gmail.com>
2022-08-27 23:34:12 -0600

    vowelCount solution added

---
+++ vowelCount.js
@@ -0,0 +1,22 @@
+/*
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+ */
+
+// JavaScript Solution
+function getCount(str) {
+    var vowelsCount = 0;
+    str = str.toLowerCase().split('')
+    const vowels = ["a", "e", "i", "o", "u"]
+
+    for(let i = 0; i < str.length; i++){
+        if(vowels.includes(str[i])){
+            vowelsCount++
+        }
+    }
+    return vowelsCount
+
+}
